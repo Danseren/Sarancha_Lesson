e55c18b965d6109045e72d651f3e64c6d544994f
Denis <etlendo@mail.ru>
2023-02-08 14:19:58 +0500

    feature_random_user | Добавил data-класс User, список юзеров и обработку нажатия на кнопку btnShowUser (показывает информацию о случайном юзере из списка)

---
+++ app/src/main/java/ru/aston/sarancha_lesson1/User.kt
@@ -0,0 +1,8 @@
+package ru.aston.sarancha_lesson1
+
+data class User(
+    val name: String,
+    val surname: String,
+    val age: Int,
+    val email: String
+)
--- app/src/main/java/ru/aston/sarancha_lesson1/UserFragment.kt
+++ app/src/main/java/ru/aston/sarancha_lesson1/UserFragment.kt
@@ -27,6 +27,24 @@ class UserFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val userList = arrayListOf(
+            User("John", "Doe", 30, "john.doe@gmail.com"),
+            User("Bob", "Anderson", 27, "bob1984@gmail.com"),
+            User("Jain", "Doe", 35, "jain@gmail.com"),
+            User("Bill", "Smith", 22, "bill.smith@gmail.com"),
+            User("Artur", "Doe", 47, "artur.the.king@gmail.com"),
+            User("Jack", "Doe", 33, "simple.jack@gmail.com")
+        )
+
+        with(binding) {
+            btnShowUser.setOnClickListener {
+                val randomNumber = (0..5).random()
+                tvUserName.text = "${userList[randomNumber].name} ${userList[randomNumber].surname}"
+                tvUserAge.text = "Age: ${userList[randomNumber].age}"
+                tvUserEmail.text = "Email: ${userList[randomNumber].email}"
+            }
+        }
     }
 
     override fun onDestroyView() {
