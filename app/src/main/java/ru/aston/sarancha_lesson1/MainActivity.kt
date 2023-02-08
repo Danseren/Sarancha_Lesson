87bbd98491eac83bbb9664d2c8eaae039740c3ff
Denis <etlendo@mail.ru>
2023-02-08 13:48:39 +0500

    Master | Подключил Binding, добавил фрагмент

--- app/src/main/java/ru/aston/sarancha_lesson1/MainActivity.kt
+++ app/src/main/java/ru/aston/sarancha_lesson1/MainActivity.kt
@@ -2,10 +2,21 @@ package ru.aston.sarancha_lesson1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.aston.sarancha_lesson1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, UserFragment.newInstance())
+                .commitNow()
+        }
     }
 }
