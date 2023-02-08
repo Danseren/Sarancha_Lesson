6a4d7c41b6acb07b713c818abaa21b7ef27b9ce5
Denis <etlendo@mail.ru>
2023-02-08 14:48:58 +0500

    feature_make_design | Добавил обработку нажатия на кнопку btnMakeDesign (меняет дизайн текстового поля)

--- app/src/main/java/ru/aston/sarancha_lesson1/UserFragment.kt
+++ app/src/main/java/ru/aston/sarancha_lesson1/UserFragment.kt
@@ -1,5 +1,6 @@
 package ru.aston.sarancha_lesson1
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -27,7 +28,27 @@ class UserFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        with(binding) {
+
+            btnMakeDesign.setOnClickListener {
+                tvUserName.apply {
+                    val randomStyle = (4..8).random()
+                    textSize = (randomStyle * 4).toFloat()
+                    setPadding(5, 25 * randomStyle, 5, 25 * randomStyle)
+                    when (randomStyle) {
+                        4 -> {setTextColor(Color.BLACK)}
+                        5 -> {setTextColor(Color.RED)}
+                        6 -> {setTextColor(Color.GREEN)}
+                        7 -> {setTextColor(Color.BLUE)}
+                        8 -> {setTextColor(Color.YELLOW)}
+                    }
+                }
+            }
+
+        }
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
